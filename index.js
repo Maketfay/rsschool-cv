fcb6856b705d9b0dbeb657abe7d1114ee3c4efe6
Kiryl Harbacheuski <kirillgorbathevski@gmail.com>
2022-12-19 20:00:18 +0300

    feat: add theme-switcher and sticky header

--- index.js
+++ index.js
@@ -23,7 +23,43 @@ navItems.forEach(el => el.addEventListener('click', closeMenu));
 
 const btn = document.querySelector(".btn-toggle");
 
-btn.addEventListener("click", function () {
+// btn.addEventListener("click", function () {
 
-  document.body.classList.toggle("dark-theme");
-});
+//   document.body.classList.toggle("dark-theme");
+// });
+
+const ToggleTheme = () => {
+  const btn = document.getElementById('theme-toggle');
+  let themeState = 1
+  
+  const themeChange = () => {
+    if(themeState == 'dark') {
+      document.body.classList.remove(themeState)
+      themeState = 'light'
+      document.body.classList.add(themeState)
+    }
+    else {
+      document.body.classList.remove(themeState)
+      themeState = 'dark'
+      document.body.classList.add(themeState)
+    }
+  }
+
+  btn.addEventListener('click', ()=>themeChange())
+}
+
+const stickyHeader = () => {
+  const header = document.getElementsByTagName('header')[0];
+
+  console.log(window.scrollY)
+  if(window.scrollY > 100) {
+    header.classList.add('sticky')
+  }
+  else {
+    header.classList.remove('sticky')
+  }
+}
+
+
+document.addEventListener('scroll', stickyHeader)
+document.addEventListener('DOMContentLoaded', ToggleTheme)
